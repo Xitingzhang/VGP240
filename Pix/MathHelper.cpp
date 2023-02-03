8aee5e8dd5231832bfe9c3c8f1b5e82f45fce6df
Xitingzhang <77800909+Xitingzhang@users.noreply.github.com>
2023-02-03 11:04:41 +0800

    add 4

--- Pix/MathHelper.cpp
+++ Pix/MathHelper.cpp
@@ -51,3 +51,34 @@ Vector3 MathHelper::Cross(const Vector3& a, const Vector3& b)
 		a.z * b.x - a.x * b.z,
 		a.x * b.y - a.y * b.x);
 }
+Vector3 MathHelper::TransformCoord(const Vector3& v, const Matrix4& m)
+{
+
+}
+Vector3 MathHelper::TransformNormal(const Vector3& v, const Matrix4& m)
+{
+
+}
+float MathHelper::Determinant(const Matrix4 m)
+{
+
+}
+Matrix4 MathHelper::Adjoint(const Matrix4& m) 
+{
+
+}
+Matrix4 MathHelper::Transpose(const Matrix4& m) 
+{
+	return Matrix4(
+		m._11, m._21, m._31, m._41,
+		m._12, m._22, m._32, m._42,
+		m._13, m._23, m._33, m._43,
+		m._14, m._24, m._34, m._44
+		);
+}
+Matrix4 MathHelper::Inverse(const Matrix4& m) 
+{
+	const float determinant = Determinant(m);
+	const float invDet = 1.0f / determinant;
+	return Adjoint(m) * invDet;
+}
