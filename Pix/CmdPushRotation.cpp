7e244938ad1f1923c01ade401379b4f7d6eda661
Xitingzhang <77800909+Xitingzhang@users.noreply.github.com>
2023-02-24 10:18:10 +0800

    add light

--- Pix/CmdPushRotation.cpp
+++ Pix/CmdPushRotation.cpp
@@ -1,13 +1,21 @@
 #include "CmdPushRotationY.h"
-#include "Variablecache.h"
-#include "Matrixstack.h"
+#include "MatrixStack.h"
+#include "VariableCache.h"
+
+#define PI 3.1415926
+
 bool CmdPushRotationY::Execute(const std::vector<std::string>& params)
 {
-	if (params.size() < 1)
-		return false;
-
-	auto degree = VariableCache::Get()->GetFloat(params[0]);
-
-	MatrixStack::Get()->PushRotationY(degree * 3.1416f / 180.0f);
-	return true;
+    if (params.size() != 1) {
+        return false;
+    }
+    float radian;
+    if (params[0][0] == '$') {
+        radian = VariableCache::Get()->GetFloat(params[0]) * PI / 180;
+    }
+    else {
+        radian = std::stof(params[0]) * PI / 180;
+    }
+    MatrixStack::Get()->PushRotationY(radian);
+    return true;
 }
--- Pix/CmdPushRotationX.cpp
+++ Pix/CmdPushRotationX.cpp
@@ -1,12 +1,21 @@
 #include "CmdPushRotationX.h"
-#include "Variablecache.h"
-#include "Matrixstack.h"
+#include "MatrixStack.h"
+#include "VariableCache.h"
+
+#define PI 3.1415926
+
 bool CmdPushRotationX::Execute(const std::vector<std::string>& params)
 {
-	if (params.size() < 1)
-		return false;
-
-	auto degree = VariableCache::Get()->GetFloat(params[0]);
-	MatrixStack::Get()->PushRotationX(degree * 3.1416f / 180.0f);
-	return true;
+    if (params.size() != 1) {
+        return false;
+    }
+    float radian;
+    if (params[0][0] == '$') {
+        radian = VariableCache::Get()->GetFloat(params[0]) * PI / 180;
+    }
+    else {
+        radian = std::stof(params[0]) * PI / 180;
+    }
+    MatrixStack::Get()->PushRotationX(radian);
+    return true;
 }
